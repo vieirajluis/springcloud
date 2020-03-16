fae9fa1370c11655221992ce6144920febc8eec3
vieirajluis <joaolsvieira@gmail.com>
2020-03-16 10:14:35 -0400

    Add

---
+++ configserver-demo/src/test/java/com/example/configserverdemo/ConfigserverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
